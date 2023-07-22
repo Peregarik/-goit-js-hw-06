94dfd7e9fd1754d4afc0ae88378df7b17f69d70a
Peregarik <rogi0014@gmail.com>
2023-07-22 17:58:25 +0300

    fix 6- 7 task

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,10 @@
-const textInput = document.querySelector("#validation-input");
-console.log(textInput.getAttribute("data-length"));
-textInput.addEventListener("blur", (event) => {
-  if (event.target.value.length == textInput.getAttribute("data-length")) {
-    textInput.classList.add("valid");
-    if (textInput.classList.contains("invalid")) {
-      textInput.classList.remove("invalid");
+const textInput = (document.getElementById("validation-input").onblur =
+  function () {
+    if (this.getAttribute("data-length") > this.value.length) {
+      this.classList.remove("valid");
+      this.classList.add("invalid");
+    } else {
+      this.classList.remove("invalid");
+      this.classList.add("valid");
     }
-  } else {
-    if (textInput.classList.contains("valid")) {
-      textInput.classList.remove("valid");
-    }
-    textInput.classList.add("invalid");
-  }
-});
+  });
